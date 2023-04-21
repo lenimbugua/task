e5129713772409c25c716bdbdb58f075c1802d0a
lenimbugua <lenimbugua@gmail.com>
2023-04-21 05:04:53 +0300

    modified relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
 class User extends Authenticatable
@@ -49,8 +49,8 @@ class User extends Authenticatable
     /**
      * Get all tasks for the user.
      */
-    public function tasks(): HasManyThrough
+    public function tasks(): HasMany
     {
-        return $this->hasManyThrough(Task::class, UserTask::class);
+        return $this->hasMany( UserTask::class);
     }
 }
--- app/Models/UserTask.php
+++ app/Models/UserTask.php
@@ -12,10 +12,26 @@ class UserTask extends Model
     use HasFactory;
 
     /**
+     * Get the user that owns the user-task.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
      * Get the task that owns the user-task.
      */
     public function task(): BelongsTo
     {
         return $this->belongsTo(Task::class);
+    }
+
+    /**
+     * Get the status that owns the user-task.
+     */
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(Status::class);
     }
 }
